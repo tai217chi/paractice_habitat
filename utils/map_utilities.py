9f2dc2fe8a5d6b56ab608f7551283831005d1729
sakaguchita <sakaguchi.taichi@em.ci.ritsumei.ac.jp>
2023-03-14 09:31:57 +0900

    [FIX] fix the function for visualize search points on 2D Map

--- utils/map_utilities.py
+++ utils/map_utilities.py
@@ -50,26 +50,43 @@ def _save_robot_trajectory(top_down_map, key_points=None, num_search = 0) -> Non
 #===============================================================================
 # すべての探索候補点を2次元の地図上に描画する関数
 #===============================================================================
-def _draw_all_points_on_map(sim: habitat_sim.Simulator, search_points: np.ndarray) -> None:
+def draw_all_points_on_map(sim: habitat_sim.Simulator, search_points: np.ndarray, meters_per_pixel: float=0.02) -> None:
     
     save_dir = Path(__file__).parent.resolve() / "map"
     
+    height = sim.pathfinder.get_bounds()[0][1] # 地図の高さを取得
+    
     ## 2次元の地図を生成 ##
-    top_down_map = maps.get_topdown_map(
-        sim,
-        map_resolution=0.1,
-        include_agent_map=True,
-        include_collision_map=True,
-        include_semantic_map=True,
-    )
+    top_down_map = sim.pathfinder.get_topdown_view(meters_per_pixel, height)
+    print(top_down_map.shape)
+    
+    grid_dimension = (top_down_map.shape[0], top_down_map.shape[1])
+    
+    ## 探索候補点の座標系を変換 ##
+    points_map_index = _convert_world_coordinate_to_map_index(sim.pathfinder, search_points, meters_per_pixel)
     
     ## 探索候補点を地図上に描画 ##
     plt.figure(figsize=(12, 8))
     ax = plt.subplot(1, 1, 1)
     ax.axis("off") 
     plt.imshow(top_down_map)
-    if search_points is not None: 
-        for point in search_points:
-            plt.scatter(point[0], point[1], marker="o", s=10, alpha=0.8)
+    if points_map_index is not None: 
+        for point_map_index in points_map_index:
+            plt.scatter(point_map_index[0], point_map_index[1], marker="o", s=30, c="black", alpha=0.8)
             
     plt.savefig(str(save_dir / "all_points"))
+    
+#===============================================================================
+# 世界座標系の探索候補点を地図のインデックスに変換する関数
+#===============================================================================
+def _convert_world_coordinate_to_map_index(pathfinder, points_world_coordinate, meters_per_pixel) -> list:
+    
+    points_map_index = [] 
+    bounds = pathfinder.get_bounds()
+    for point_world_coordinate in points_world_coordinate:
+        # convert world coordinate x, z to mapindex x, y
+        px = (point_world_coordinate[0] - bounds[0][0]) / meters_per_pixel
+        py = (point_world_coordinate[2] - bounds[0][2]) / meters_per_pixel
+        points_map_index.append(np.array([px, py]))
+        
+    return points_map_index
