30fd0bcbfe81ef37edaec7f765f0561d7e861669
sakaguchita <sakaguchi.taichi@em.ci.ritsumei.ac.jp>
2023-03-16 16:23:50 +0900

    [FIX] fix path for json file

--- config/simulator_configure.py
+++ config/simulator_configure.py
@@ -5,13 +5,13 @@ from pathlib import Path
 class MatterportConfig:
     
     ## configuraton for observation image resolution ##
-    width = 320
-    height = 240
+    width = 640
+    height = 480
     
     ## specify dataset ##
     _dataset_path = Path(__file__).parents[1] / "data"
-    scene = _dataset_path / "scene_datasets" / "mp3d" / "29hnd4uzFmX" / "29hnd4uzFmX.glb"
-    mp3d_scene_dataset = _dataset_path / "scene_datasets" / "mp3d" / "mp3d.scene_dataset_config.json"
+    scene = _dataset_path / "scene_datasets" / "mp3d" / "2azQ1b91cZZ" / "2azQ1b91cZZ.glb"
+    mp3d_scene_dataset = _dataset_path / "scene_datasets" / "mp3d.scene_dataset_config.json"
     
     ## configuration for observation kind ##
     rgb_sensor = True
