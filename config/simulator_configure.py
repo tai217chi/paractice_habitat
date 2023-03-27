08f78c9e925636ee4bc296c040fdcb30c3d1df96
sakaguchita <sakaguchi.taichi@em.ci.ritsumei.ac.jp>
2023-03-27 16:10:07 +0900

    [ADD] add new config class for HM3D and Replica Dataset

--- config/simulator_configure.py
+++ config/simulator_configure.py
@@ -12,7 +12,7 @@ class MatterportConfig:
     ## specify dataset ##
     _dataset_path = Path(__file__).parents[1] / "data"
     scene = _dataset_path / "scene_datasets" / "mp3d" / "2azQ1b91cZZ" / "2azQ1b91cZZ.glb"
-    mp3d_scene_dataset = _dataset_path / "scene_datasets" / "mp3d.scene_dataset_config.json"
+    scene_dataset_config = _dataset_path / "scene_datasets" / "mp3d.scene_dataset_config.json"
     
     ## configuration for observation kind ##
     rgb_sensor = True
@@ -28,11 +28,30 @@ class MatterportConfig:
     enable_physics = False
     
 class HM3DConfig:
-    """
-    TODO: write configuration for HM3D dataset
     
-    """    
-    pass
+    dataset_kind = "val"
+    
+    ## configuration for observation images (RGB, instance mask, Depth) ##
+    width = 640
+    height = 480
+    
+    ## specify dataset ##
+    _dataset_path = Path(__file__).parents[1] / "data" 
+    scene = _dataset_path / "scene_datasets" / "hm3d" / f"{dataset_kind}" /"00800-TEEsavR23oF" / "TEEsavR23oF.basis.glb"
+    scene_dataset_config = _dataset_path / "scene_datasets" / "hm3d_annotated_val_basis.scene_dataset_config.json"
+    
+    ## configuration for observation kind ##
+    rgb_sensor = True 
+    depth_sensor = True 
+    semantic_sensor = True 
+    
+    ## configuration for parameters related to embodied agent ##
+    default_agent = 0 
+    sensor_height = 0.8
+    
+    ## other configuration ##
+    seed = 1
+    enable_physics = False
 
 class ReplicaConfig:
     
@@ -42,8 +61,8 @@ class ReplicaConfig:
     
     ## specify dataset ##
     _dataset_path = Path(__file__).parents[1] / "data" 
-    scene = _dataset_path / "scene_datasets" / "replica_v1" / "apartment_0" / "habitat" /"mesh_semantic.ply"
-    replica_scene_dataset = _dataset_path / "scene_datasets" / "replica_v1.scene_dataset_config.json"
+    scene = _dataset_path / "scene_datasets" / "replica_v1" / "apartment_1" / "habitat" /  "mesh_semantic.ply"
+    scene_dataset_config = _dataset_path / "scene_datasets" / "replica_v1.scene_dataset_config.json"
     
     ## configuration for observation kind ##
     rgb_sensor = True 
